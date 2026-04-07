8ee3532c52e2237b0b9e246d571a3f8ed8a1eb2a
yebof <fengyebo@gmail.com>
2026-04-08 00:03:22 +0800

    fix: remove .venv from git, update .gitignore

--- .venv/lib/python3.11/site-packages/alpaca/common/types.py
+++
@@ -1,7 +0,0 @@
-from typing import Any, Dict, List, Optional, Tuple, Union
-
-RawData = Dict[str, Any]
-
-# TODO: Refine this type
-HTTPResult = Union[dict, List[dict], Any]
-Credentials = Tuple[Optional[str], Optional[str], Optional[str]]
--- .venv/lib/python3.11/site-packages/sseclient/version.py
+++
@@ -1,2 +0,0 @@
-name = "sseclient-py"
-version = "1.9.0"
